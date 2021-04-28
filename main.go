52c9b6a79293564cf4f35018657bbb2531ac5a53
Milosz A. W <miloszaw@stud.ntnu.no>
2021-04-28 12:20:02 +0200

    Added repo initialization in case of missing repo, migration problems

--- main.go
+++ main.go
@@ -3,15 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	config "github.com/ipfs/go-ipfs-config"
 	"github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	"io/ioutil"
 )
+
+const repoPath = "./.ipfs"
 
 func main() {
 	// Basic ipfsnode setup
-	r, err := fsrepo.Open("~/.ipfs")
+	r, err := fsrepo.Open(repoPath) // Attempts to open IPFS repo
 	if err != nil {
-		panic(err)
+		if fsrepo.IsInitialized(repoPath) { // Checks if error comes from uninitialized repo
+			panic(err)
+		} else {
+
+			// Create a config with default options and a 2048 bit key
+			cfg, err := config.Init(ioutil.Discard, 2048)
+			if err != nil {
+				panic(err)
+			}
+			// Initialize repo at path
+			err = fsrepo.Init(repoPath, cfg)
+			if err != nil {
+				panic(err)
+			}
+		}
+
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
