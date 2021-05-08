71c2c47c45b66634cf62c42b3fb2431dc6cf12b5
Milosz A. W <miloszaw@stud.ntnu.no>
2021-05-08 20:29:17 +0200

    updated the bootstrap.go with new functions

--- bootstrap.go
+++ bootstrap.go
@@ -32,3 +32,33 @@ func addBootstrap(addresses []string) error {
 	}
 
 }
+
+func removeBootstrap() {
+
+}
+
+func clearBootstrap() error {
+	if fsrepo.IsInitialized(repoPath) { // Checks if repo is initialized
+		nodeRepo, err := fsrepo.Open(repoPath) // Opens repo
+		if err != nil {
+			return fmt.Errorf("failed to open repo when adding bootstrap: %s", err)
+		}
+
+		var cfg *config.Config // Defines config file
+
+		cfg, err = nodeRepo.Config() // Receives current config
+		if err != nil {
+			return fmt.Errorf("failed to open repo when adding bootstrap: %s", err)
+		}
+
+		cfg.Bootstrap = nil
+
+		err = nodeRepo.SetConfig(cfg) // Sets current config with updated config
+		if err != nil {
+			return fmt.Errorf("failed to set config when adding bootstrap: %s", err)
+		}
+		return nil
+	} else {
+		return fmt.Errorf("cannot add bootstrap to an uninitialized node")
+	}
+}
